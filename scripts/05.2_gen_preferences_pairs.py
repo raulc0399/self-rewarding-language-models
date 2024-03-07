6a998738a763a71d951daf5882a31d556647dea0
raul <raul@netzinkubator.de>
2024-03-07 19:25:30 +0200

    preferences pairs in format for dpo

--- scripts/05.2_gen_preferences_pairs.py
+++ scripts/05.2_gen_preferences_pairs.py
@@ -17,12 +17,15 @@ for index, row in df.iterrows():
     # Check if there are at least two different completions to avoid duplicating the same entry
     if len(sorted_completions) > 1:
         # Keep only the completions with the minimum and maximum scores
-        min_max_completions = [sorted_completions[0], sorted_completions[-1]]
+        min_max_completions = {"rejected": sorted_completions[0]["completion"], "chosen": sorted_completions[-1]["completion"]}
     else:
         # In case there's only one completion, keep it as it is
-        min_max_completions = sorted_completions
+        min_max_completions = {"chosen": sorted_completions[-1]["completion"]}
     
     # Update the 'completions' in the DataFrame
-    df.at[index, 'completions'] = min_max_completions
+    df.at[index, 'chosen'] = min_max_completions['chosen']
+    df.at[index, 'rejected'] = min_max_completions['rejected'] if 'rejected' in min_max_completions else ""
+
+df = df.drop(columns=['completions'])
 
 df.to_json(preferences_pairs_file_path, orient='records', lines=True)
